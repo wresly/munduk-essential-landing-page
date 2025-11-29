c097e52184597cccc3492960da13107f3ebf430f
Kadek Chuk wresly <wreslyroyale1717@gmail.com>
2025-11-29 15:39:24 +0800

    room

--- app/components/rooms.tsx
+++ app/components/rooms.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "@/components/ui/carousel"
+import { House, Bath, BedDouble } from "lucide-react";
 
 export default function Rooms() {
     return (
@@ -32,38 +33,29 @@ export default function Rooms() {
 
                                     <div className="mt-6 flex items-center gap-8 text-xs">
                                         <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                                            <svg className="size-4 text-indigo-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"></path>
-                                            </svg>
-
+                                            <House className="size-6 text-indigo-700" />
                                             <div className="mt-1.5 sm:mt-0">
-                                                <p className="text-gray-500">Parking</p>
+                                                <p className="text-gray-500">Room Area</p>
 
-                                                <p className="font-medium">2 spaces</p>
+                                                <p className="font-medium">1000 Sqm</p>
                                             </div>
                                         </div>
 
                                         <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                                            <svg className="size-4 text-indigo-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path>
-                                            </svg>
-
+                                            <Bath className="size-6 text-indigo-700" />
                                             <div className="mt-1.5 sm:mt-0">
                                                 <p className="text-gray-500">Bathroom</p>
 
-                                                <p className="font-medium">2 rooms</p>
+                                                <p className="font-medium">1 Room</p>
                                             </div>
                                         </div>
 
                                         <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                                            <svg className="size-4 text-indigo-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path>
-                                            </svg>
-
+                                            <BedDouble className="size-6 text-indigo-700" />
                                             <div className="mt-1.5 sm:mt-0">
-                                                <p className="text-gray-500">Bedroom</p>
+                                                <p className="text-gray-500">Bed</p>
 
-                                                <p className="font-medium">4 rooms</p>
+                                                <p className="font-medium">1 Queen Bed</p>
                                             </div>
                                         </div>
                                     </div>
@@ -91,38 +83,29 @@ export default function Rooms() {
 
                                     <div className="mt-6 flex items-center gap-8 text-xs">
                                         <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                                            <svg className="size-4 text-indigo-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"></path>
-                                            </svg>
-
+                                            <House className="size-6 text-indigo-700" />
                                             <div className="mt-1.5 sm:mt-0">
-                                                <p className="text-gray-500">Parking</p>
+                                                <p className="text-gray-500">Room Area</p>
 
-                                                <p className="font-medium">2 spaces</p>
+                                                <p className="font-medium">1000 Sqm</p>
                                             </div>
                                         </div>
 
                                         <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                                            <svg className="size-4 text-indigo-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path>
-                                            </svg>
-
+                                            <Bath className="size-6 text-indigo-700" />
                                             <div className="mt-1.5 sm:mt-0">
                                                 <p className="text-gray-500">Bathroom</p>
 
-                                                <p className="font-medium">2 rooms</p>
+                                                <p className="font-medium">1 Room</p>
                                             </div>
                                         </div>
 
                                         <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-                                            <svg className="size-4 text-indigo-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path>
-                                            </svg>
-
+                                            <BedDouble className="size-6 text-indigo-700" />
                                             <div className="mt-1.5 sm:mt-0">
-                                                <p className="text-gray-500">Bedroom</p>
+                                                <p className="text-gray-500">Bed</p>
 
-                                                <p className="font-medium">4 rooms</p>
+                                                <p className="font-medium">1 Queen Bed</p>
                                             </div>
                                         </div>
                                     </div>
